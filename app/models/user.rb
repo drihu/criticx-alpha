3eaaf0d57021f670f4574e56c2d50b1c37da790f
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-05-29 01:33:16 -0500

    add authentication with devise gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :reviews
 
   validates :username, :email, presence: true, uniqueness: true
@@ -6,7 +10,7 @@ class User < ApplicationRecord
 
   private
   def older_than_16
-    if birth_date > Date.today - 16.year
+    if birth_date >= 16.year.ago.to_date
       errors.add(:birth_date, 'You should be 16 years old to create an account')
     end
   end
